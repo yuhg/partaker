e183c4e0e49c9d3e9bb61a3c9ecab90737d2df7e
yuhg <2011yhg@gmail.com>
2013-12-19 19:42:55 +0800

    splash
    add new activity

---
+++ src/com/fb/partaker/MainActivity.java
@@ -0,0 +1,17 @@
+package com.fb.partaker;
+
+import android.os.Bundle;
+
+public class MainActivity extends FBActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+	}
+
+}
--- src/com/fb/partaker/Splash.java
+++ src/com/fb/partaker/Splash.java
@@ -1,14 +1,45 @@
 package com.fb.partaker;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 public class Splash extends FBActivity{
+	private Context myContext;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash);
+		myContext = getApplicationContext();
+		new AsyncTaskonResume().execute(myContext);
 	}
 
+	private class AsyncTaskonResume extends
+	AsyncTask<Context, Integer, Integer> {
+
+		@Override
+		protected void onPostExecute(Integer result) {
+			super.onPostExecute(result);
+			Intent intent = new Intent();
+			intent.setClass(myContext, MainActivity.class);
+			startActivity(intent);
+		}
+
+		@Override
+		protected Integer doInBackground(Context... arg0) {
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			return null;
+		}
+		
+	}
+	
+	
 	@Override
 	protected void onDestroy() {
 		super.onDestroy();
