c6e03cfe9d92f0444d2943646a3040f2921965f9
yuhg <121593998@qq.com>
2013-12-25 23:49:08 +0800

    new
    new

--- gen/com/fb/partaker/R.java
+++ gen/com/fb/partaker/R.java
@@ -21,13 +21,26 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int bg=0x7f020000;
+        public static final int book_sea=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int login=0x7f020003;
+        public static final int logo=0x7f020004;
+        public static final int menu=0x7f020005;
+        public static final int search=0x7f020006;
+        public static final int splash=0x7f020007;
+        public static final int title_bg=0x7f020008;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080003;
+        public static final int logo=0x7f080000;
+        public static final int menu=0x7f080001;
+        public static final int search=0x7f080002;
     }
     public static final class layout {
         public static final int login=0x7f030000;
+        public static final int splash=0x7f030001;
+        public static final int title=0x7f030002;
     }
     public static final class menu {
         public static final int login=0x7f070000;
